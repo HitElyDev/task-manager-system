c47a9e3f10d830e48794e6839a7fe5111c862ee3
htrujilloHome <hector.trujillo.dotnet@gmail.com>
2025-12-07 04:27:14 -0600

    [HITG] new: Adding Entities and Enums Folders with HTaskItem and HTaskStatus classes

---
+++ HTask.Domain/Entities/HTaskItem.cs
@@ -0,0 +1,21 @@
+﻿using HTask.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTask.Domain.Entities
+{
+    public class HTaskItem
+    {
+        
+        public int Id { get; set; }
+        
+        public string Title { get; set; } = string.Empty;
+       
+        public string? Description { get; set; }
+        
+        public HTaskStatus Status { get; set; } = HTaskStatus.Pending;
+        
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ HTask.Domain/Enums/HTaskStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTask.Domain.Enums
+{
+    // Requisito: Status (enumeración: Pending, InProgress, Completed)
+    public enum HTaskStatus
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+}
