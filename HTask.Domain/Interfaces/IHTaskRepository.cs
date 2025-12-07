7dcb8ce1d9eeb99c9d775fe18290e4f2a65cfd3e
htrujilloHome <hector.trujillo.dotnet@gmail.com>
2025-12-07 04:42:39 -0600

    [HITG] new: "Adding Interfaces folder with IHTaskRepository and IUnitOfWork Interfaces classes

---
+++ HTask.Domain/Interfaces/IHTaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HTask.Domain.Entities;
+
+
+namespace HTask.Domain.Interfaces
+{
+    // Abstracción que define las operaciones de datos
+    public interface IHTaskRepository
+    {
+        Task<HTaskItem?> GetByIdAsync(int id);
+        Task<IEnumerable<HTaskItem>> GetAllAsync();
+        void Add(HTaskItem task);
+        void Update(HTaskItem task);
+        void Remove(HTaskItem task);
+    }
+}
---
+++ HTask.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HTask.Domain.Interfaces;
+
+namespace HTask.Domain.Interfaces
+{
+    // Contrato para manejar las transacciones
+    public interface IUnitOfWork : IDisposable
+    {
+        // Exposición de Repositorios
+        IHTaskRepository Tasks { get; }
+
+        // Método para guardar todos los cambios pendientes
+        Task<int> CompleteAsync();
+    }
+}
